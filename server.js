a47a09719ba847cf7b9e23e097aeadf8a624e46b
Vinícius de Oliveira <vi.o@hotmail.com>
2020-03-31 15:25:39 -0300

    create dist folder after ng build prod

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const nomeApp = process.env.npm_package_name;
 const app = express();
  
-app.use(express.static(__dirname + '/src'));
+app.use(express.static("./dist/angular-project"));
  
-app.get('/*', (req, res) => {
-res.sendFile(path.join(__dirname + '/src/index.html'));
-});
+app.get("/*", function(req, res) {
+    res.sendFile("index.html", {root: "dist/<name-on-package.json>/"}
+  );
+  });
  
 app.listen(process.env.PORT || 8080);
